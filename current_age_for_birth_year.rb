611fb9f7fd2710a94bbe617f7ac987d3c26a67c8
Kechi Boniface <bonifacekechi@gmail.com>
2020-08-20 21:05:06 +0000

    Automatically backed up by Learn

--- current_age_for_birth_year.rb
+++ current_age_for_birth_year.rb
@@ -1,9 +1,2 @@
-require_relative '../current_age_for_birth_year.rb'
-
-describe "current_age_for_birth_year method" do
-  it "returs the age of a perosn based on the year of the birth" do
-
-
-    expect (age_of_person).to eq(19)
-  end
+def current_age_for_birth_year(birth_year)
 end
